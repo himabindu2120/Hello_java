975c4933a05e1ea33eb1c3e5a5b8e6192be47287
himabindu2120 <161450579+himabindu2120@users.noreply.github.com>
2024-02-27 15:32:01 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-public class Hello {
-  public static void main(String[] args) {
-    System.out.println("Hello,world");
-  }
-}   
+lass Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}  
